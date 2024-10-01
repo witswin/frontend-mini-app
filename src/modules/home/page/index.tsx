f0e5ee40884ae4040e5d3c72ba799f2228d8d0a9
AmirhoseinShahrizi <amirhoseinshahrizi1378@gmail.com>
2024-10-01 14:29:14 +0330

    fix: update Home page logic

--- src/modules/home/page/index.tsx
+++ src/modules/home/page/index.tsx
@@ -3,14 +3,45 @@ import { CARD_STATE, INFO_CARD_STATE } from "@/types";
 import { VStack } from "@chakra-ui/react";
 import { HomeCard } from "../components/HomeCard";
 import { TopNavbar } from "@/components/TopNavbar";
+import { useMemo, useState } from "react";
 
 export const Home = () => {
+  const [homeState, setHomeState] = useState(INFO_CARD_STATE.watch);
+
+  const infoCard = useMemo(() => {
+    return {
+      [INFO_CARD_STATE.welcome]: <HomeCard state={INFO_CARD_STATE.welcome} />,
+      [INFO_CARD_STATE.join]: (
+        <>
+          <HomeCard state={INFO_CARD_STATE.join} />
+          <QuizCard state={CARD_STATE.join} colored />
+        </>
+      ),
+      [INFO_CARD_STATE.lobby]: (
+        <>
+          <HomeCard state={INFO_CARD_STATE.lobby} />
+          <QuizCard state={CARD_STATE.lobby} colored />
+        </>
+      ),
+      [INFO_CARD_STATE.resource]: (
+        <>
+          <HomeCard state={INFO_CARD_STATE.resource} />
+          <QuizCard state={CARD_STATE.resource} colored />
+        </>
+      ),
+      [INFO_CARD_STATE.watch]: (
+        <>
+          <HomeCard state={INFO_CARD_STATE.watch} />
+          <QuizCard state={CARD_STATE.watch} colored />
+        </>
+      ),
+    };
+  }, []);
+
   return (
     <VStack h="full" w="full" gap="16px">
       <TopNavbar />
-      <HomeCard state={INFO_CARD_STATE.welcome} />
-
-      {/* <QuizCard state={CARD_STATE.join} colored /> */}
+      {infoCard[homeState]}
     </VStack>
   );
 };
