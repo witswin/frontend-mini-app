23b765df09dc4b759ccefc8a40358de4b768312e
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-10-02 15:21:36 +0330

    refactor: :recycle: Move container & add curcular bg

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,11 +1,20 @@
 import { EnrolledModalProvider } from "@/modules/quiz/context";
 import { QuizPLP } from "@/modules/quiz/page";
+import { Container } from "@chakra-ui/react";
 
 const Index = () => {
   return (
-    <EnrolledModalProvider>
-      <QuizPLP />
-    </EnrolledModalProvider>
+    <Container
+      overflow="hidden"
+      minH="100vh"
+      py="8px"
+      maxWidth="538px"
+      px="16px"
+    >
+      <EnrolledModalProvider>
+        <QuizPLP />
+      </EnrolledModalProvider>
+    </Container>
   );
 };
 
