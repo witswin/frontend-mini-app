5264d28b5355d45a8d8be778ffe946bbe91c736d
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-10-15 15:00:29 +0330

    feat: :sparkles: Add question page

--- src/modules/question/page/index.tsx
+++ src/modules/question/page/index.tsx
@@ -1,10 +1,78 @@
-import { VStack } from "@chakra-ui/react";
+import { Box, HStack, Text, VStack } from "@chakra-ui/react";
 import { QuestionCard } from "../components/QuestionCard";
+import { Card } from "@/components/Card";
+import { AnimatePresence, motion } from "framer-motion";
+import { useQuestionData } from "../hooks";
+import { QuestionBanner } from "../components/QuestionBanner";
+import { QUESTION_STATE } from "@/types";
+import { ChoiceButton } from "@/components/ChoiceButton";
+import { ProgressTimer } from "@/components/ProgressTimer";
 
 export const Question = () => {
+  const { questions, activeQuestionId } = useQuestionData();
+  const activeQuestion = questions.find((item) => item.id === activeQuestionId);
+
   return (
-    <VStack width="full">
-      <QuestionCard />
+    <VStack height="full" position="relative" width="full">
+      {questions.map((item, index, array) => (
+        <Box
+          zIndex={-1}
+          position="absolute"
+          top={`${index * 8}px`}
+          left="50%"
+          transform="translateX(-50%)"
+          width={`calc(100% - ${(array.length - index) * 20}px)`}
+          key={item.id}
+        >
+          <Card height="full" minH="200px">
+            <Box filter="blur(4px)" width="full">
+              <QuestionBanner content={activeQuestion.title} />
+              <ProgressTimer
+                timer={0}
+                state={QUESTION_STATE.default}
+                hasCounter
+                hasIcon
+              />
+              {activeQuestion.choices.map((choice) => (
+                <ChoiceButton
+                  setSelectedChoice={undefined}
+                  selectedChoice={undefined}
+                  key={choice.id}
+                  buttonInfo={choice}
+                />
+              ))}
+              <Text color="gray.200" fontSize="xs">
+                By Adams Sandler
+              </Text>
+            </Box>
+          </Card>
+        </Box>
+      ))}
+      <AnimatePresence mode="popLayout">
+        <motion.div
+          key={activeQuestion.id}
+          style={{
+            paddingTop: `${questions.length * 8}px`,
+            width: "100%",
+          }}
+          initial={{
+            opacity: 0,
+            scale: 0,
+          }}
+          animate={{
+            scale: 1,
+            opacity: 1,
+          }}
+          exit={{
+            y: 500,
+            opacity: 0,
+          }}
+          transition={{ duration: 2, ease: [0.43, 0.13, 0.23, 0.96] }}
+        >
+          <QuestionCard />
+        </motion.div>
+      </AnimatePresence>
+      <HStack></HStack>
     </VStack>
   );
 };
--- src/pages/quiz/[id]/[question].tsx
+++ src/pages/quiz/[id]/[question].tsx
@@ -1,12 +1,35 @@
 import { QuestionDataProvider } from "@/modules/question/context";
 import { Question } from "@/modules/question/page";
+import { Box, Container } from "@chakra-ui/react";
+import { ReactElement } from "react";
 
 const Index = () => {
   return (
-    <QuestionDataProvider timer={10}>
+    <QuestionDataProvider timer={4}>
       <Question />
     </QuestionDataProvider>
   );
 };
 
 export default Index;
+
+Index.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Container
+      py="8px"
+      maxWidth="538px"
+      zIndex={1}
+      gap="16px"
+      display="flex"
+      height="full"
+      px="0"
+      minH="calc(100vh - 122px)"
+      justifyContent="stretch"
+      alignItems="stretch"
+    >
+      <Box px="16px" width="full">
+        {page}
+      </Box>
+    </Container>
+  );
+};
