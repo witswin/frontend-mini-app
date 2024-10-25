9ed3f2de77a324238e0c021474aef2e92b7d2196
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-10-25 14:41:14 +0330

    fix: :bug: Fix circular background

--- src/components/CircularPattern.tsx
+++ src/components/CircularPattern.tsx
@@ -1,6 +1,6 @@
 import { Box, BoxProps } from "@chakra-ui/react";
 
-const CircleShadow = ({ ...props }: BoxProps) => (
+export const CircleShadow = ({ ...props }: BoxProps) => (
   <>
     <Box
       zIndex={-1}
@@ -18,9 +18,49 @@ const CircleShadow = ({ ...props }: BoxProps) => (
 export const CircularPattern = () => {
   return (
     <Box>
-      <CircleShadow bottom="0" right="calc(50vw - 206px)" />
+      <Box
+        zIndex={0}
+        bottom="0"
+        right="calc(50vw - 206px)"
+        width="50px"
+        height="50px"
+        position="absolute"
+        filter="blur(80px)"
+        background="radial-gradient(101.43% 155% at 49.76% -9%, #9553F6 0%, #6E81EE 29.98%, #63C6E1 100%)"
+      />
+      <Box
+        zIndex={0}
+        top="60vh"
+        right="calc(50vw + 206px)"
+        width="50px"
+        height="50px"
+        position="absolute"
+        filter="blur(80px)"
+        background="radial-gradient(101.43% 155% at 49.76% -9%, #9553F6 0%, #6E81EE 29.98%, #63C6E1 100%)"
+      />
+      <Box
+        zIndex={0}
+        top="40vh"
+        right="calc(50vw - 103px)"
+        width="50px"
+        height="50px"
+        position="absolute"
+        filter="blur(80px)"
+        background="radial-gradient(101.43% 155% at 49.76% -9%, #9553F6 0%, #6E81EE 29.98%, #63C6E1 100%)"
+      />
+      <Box
+        zIndex={0}
+        top="120px"
+        right="calc(50vw + 103px)"
+        width="50px"
+        height="50px"
+        position="absolute"
+        filter="blur(80px)"
+        background="radial-gradient(101.43% 155% at 49.76% -9%, #9553F6 0%, #6E81EE 29.98%, #63C6E1 100%)"
+      />
+      {/* <CircleShadow bottom="0" right="calc(50vw - 206px)" />
       <CircleShadow top="60vh" right="calc(50vw + 206px)" />
-      <CircleShadow top="40vh" right="calc(50vw - 103px)" />
+      <CircleShadow top="40vh" right="calc(50vw - 103px)" /> */}
       {/* <CircleShadow top="120px" right="calc(50vw + 103px)" /> */}
     </Box>
   );
