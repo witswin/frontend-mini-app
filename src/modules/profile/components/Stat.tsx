c00ef6da7f5fc5076b7d657a8bc7a8955b3c0b24
amirhoseinShahrizi <amirhoseinshahrizi1378@gmail.com>
2024-11-16 18:33:17 +0330

    feature: stats section front done (connection to back remains)

---
+++ src/modules/profile/components/Stat.tsx
@@ -0,0 +1,103 @@
+import { Divider, Flex, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import { Card } from "./Card";
+import { CheckCircle, Notes, Ranking } from "solar-icon-set";
+
+interface StatProps {
+  rank: number;
+  quizzes: number;
+  winRate: number;
+}
+
+export const Stat = ({ rank, quizzes, winRate }: StatProps) => {
+  return (
+    <Card>
+      <Flex w="full" h="full" gap="8px" p="0px">
+        <VStack gap="8px" flex={1}>
+          <VStack gap="2px">
+            <Text
+              textAlign="center"
+              fontSize="2xl"
+              fontWeight={700}
+              bgGradient="var(--chakra-colors-primaryRadial)"
+              bgClip="text"
+            >
+              {rank}
+            </Text>
+            <Text
+              textAlign="center"
+              fontSize="sm"
+              color="gray.40"
+              fontWeight={800}
+            >
+              Rank
+            </Text>
+          </VStack>
+          <Ranking
+            color="var(--chakra-colors-blue)"
+            size={32}
+            iconStyle="BoldDuotone"
+          />
+        </VStack>
+
+        <Divider orientation="vertical" borderColor="gray.400" />
+
+        <VStack gap="8px" flex={1}>
+          <VStack gap="2px">
+            <Text
+              textAlign="center"
+              fontSize="2xl"
+              fontWeight={700}
+              bgGradient="var(--chakra-colors-primaryRadial)"
+              bgClip="text"
+            >
+              {quizzes}
+            </Text>
+            <Text
+              textAlign="center"
+              fontSize="sm"
+              color="gray.40"
+              fontWeight={800}
+            >
+              Quizzes Played
+            </Text>
+          </VStack>
+          <Notes
+            color="var(--chakra-colors-blue)"
+            size={32}
+            iconStyle="BoldDuotone"
+          />
+        </VStack>
+
+        <Divider orientation="vertical" borderColor="gray.400" />
+
+        <VStack gap="8px" flex={1}>
+          <VStack gap="2px">
+            <Text
+              textAlign="center"
+              fontSize="2xl"
+              fontWeight={700}
+              bgGradient="var(--chakra-colors-primaryRadial)"
+              bgClip="text"
+            >
+              {`${winRate}%`}
+            </Text>
+            <Text
+              textAlign="center"
+              fontSize="sm"
+              color="gray.40"
+              fontWeight={800}
+            >
+              Win Rate
+            </Text>
+          </VStack>
+          <CheckCircle
+            color="var(--chakra-colors-blue)"
+            size={32}
+            iconStyle="BoldDuotone"
+          />
+        </VStack>
+      </Flex>
+    </Card>
+  );
+};
--- src/modules/profile/page/index.tsx
+++ src/modules/profile/page/index.tsx
@@ -1,6 +1,11 @@
 import { VStack } from "@chakra-ui/react";
 import React from "react";
+import { Stat } from "../components/Stat";
 
 export const Profile = () => {
-  return <VStack w="full" h="full" gap="16px" p="16px"></VStack>;
+  return (
+    <VStack w="full" h="full" gap="16px" p="16px">
+      <Stat rank={1} quizzes={28} winRate={2} />
+    </VStack>
+  );
 };
