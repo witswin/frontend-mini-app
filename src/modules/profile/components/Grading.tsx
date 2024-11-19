cd382542a308019fb6ea3a22c4d6236d716b5766
amirhoseinShahrizi <amirhoseinshahrizi1378@gmail.com>
2024-11-19 14:28:05 +0330

    feat: add grades

---
+++ src/modules/profile/components/Grading.tsx
@@ -0,0 +1,114 @@
+import { HStack, Text } from "@chakra-ui/react";
+import React from "react";
+import {
+  CrownStar,
+  MedalRibbon,
+  MedalRibbonsStar,
+  MedalRibbonStar,
+  MedalStar,
+  MedalStarCircle,
+  MedalStarSquare,
+} from "solar-icon-set";
+import { Neuron } from "./icons";
+
+interface GradeType {
+  text: string;
+  color: string;
+  icon: React.JSX.Element;
+  badgeIcon: React.JSX.Element;
+}
+
+const grades = {
+  200: {
+    text: "Legend",
+    color: "#E5AF19",
+    icon: <CrownStar iconStyle="Bold" color="#E5AF19" size={24} />,
+    badgeIcon: <CrownStar iconStyle="Bold" size={20} />,
+  },
+  150: {
+    text: "Grandmasters",
+    color: "#FFA947",
+    icon: <MedalStar iconStyle="Bold" color="#FFA947" size={24} />,
+    badgeIcon: <MedalStar iconStyle="Bold" color="gray.0" size={20} />,
+  },
+  100: {
+    text: "Master",
+    color: "#F56788",
+    icon: <MedalStarSquare iconStyle="Bold" color="#F56788" size={24} />,
+    badgeIcon: <MedalStarSquare iconStyle="Bold" color="gray.0" size={20} />,
+  },
+  50: {
+    text: "Expert",
+    color: "#7B60F2",
+    icon: <MedalStarCircle iconStyle="Bold" color="#7B60F2" size={24} />,
+    badgeIcon: <MedalStarCircle iconStyle="Bold" color="gray.0" size={20} />,
+  },
+  25: {
+    text: "Student",
+    color: "#4A92FF",
+    icon: <MedalRibbonsStar iconStyle="Bold" color="#4A92FF" size={24} />,
+    badgeIcon: <MedalRibbonsStar iconStyle="Bold" color="gray.0" size={20} />,
+  },
+  10: {
+    text: "Beginner",
+    color: "#14BFCC",
+    icon: <MedalRibbonStar iconStyle="Bold" color="#14BFCC" size={24} />,
+    badgeIcon: <MedalRibbonStar iconStyle="Bold" color="gray.0" size={20} />,
+  },
+  0: {
+    text: "Trainee",
+    color: "#00BD89",
+    icon: <MedalRibbon iconStyle="Bold" color="#00BD89" size={24} />,
+    badgeIcon: <MedalRibbon iconStyle="Bold" color="gray.0" size={20} />,
+  },
+};
+
+export const getGrade = (count: number) => {
+  const key = parseInt(
+    Object.keys(grades)
+      .reverse()
+      .find((key) => parseInt(key) <= count)
+  );
+
+  return grades[key as keyof typeof grades];
+};
+
+export const GradeBadge = ({
+  grade,
+  neuronCount,
+}: {
+  grade: GradeType;
+  neuronCount: number;
+}) => {
+  return (
+    <HStack
+      padding="2px"
+      gap="4px"
+      h="34px"
+      border="1px solid"
+      borderColor={grade.color}
+      borderRadius="18px"
+    >
+      <HStack
+        p="4px"
+        gap="4px"
+        bg={grade.color}
+        borderRadius="16px"
+        h="30px"
+        alignItems="center"
+      >
+        <Text fontSize="md" fontWeight={600} color="gray.0">
+          {grade.text}
+        </Text>
+        {grade.badgeIcon}
+      </HStack>
+
+      <HStack mr="10px" ml='6px' alignItems="center" gap='4px'>
+        <Text fontSize="lg" fontWeight={800} color={grade.color}>
+          {neuronCount}
+        </Text>
+        <Neuron color={grade.color} />
+      </HStack>
+    </HStack>
+  );
+};
--- src/modules/profile/components/Info.tsx
+++ src/modules/profile/components/Info.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Card } from "./Card";
-import { Badge, Divider, Flex, HStack, Text, VStack } from "@chakra-ui/react";
+import {
+  Badge,
+  CircularProgress,
+  Divider,
+  Flex,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { SettingsMinimalistic, WalletMoney } from "solar-icon-set";
 import Link from "next/link";
 import Image from "next/image";
 import { textTruncator } from "@/utils";
 import { profileInfo } from "@/globalTypes";
 import { useAuth } from "@/hooks/useAuthorization";
+import { getGrade, GradeBadge } from "./Grading";
 
 interface Props {
   userInfo: profileInfo;
@@ -16,15 +25,40 @@ export const Info = ({ userInfo }: Props) => {
   const ownUser = useAuth();
 
   const isOwnProfile = userInfo.pk === ownUser.pk;
+  const grade = getGrade(userInfo.neuron);
 
   return (
     <Card>
-      <Image
-        alt="avatar"
-        src={userInfo?.image || "/assets/images/profile/Avatar.svg"}
-        width={88}
-        height={88}
-      />
+      <VStack position="relative" boxSize="92px" justifyContent="center">
+        <CircularProgress
+          value={40}
+          top={0}
+          left={0}
+          size="92px"
+          thickness="2px" /* Adjust border thickness */
+          color={grade.color}
+          trackColor="gray.600"
+          position="absolute"
+          transform="rotate(225deg)"
+        />
+        <Image
+          alt="avatar"
+          src={userInfo?.image || "/assets/images/profile/Avatar.svg"}
+          width={80}
+          height={80}
+        />
+        <VStack
+          boxSize="32px"
+          borderRadius="50px"
+          bg="gray.600"
+          justifyContent="center"
+          position="absolute"
+          bottom={0}
+          left={0}
+        >
+          {grade.icon}
+        </VStack>
+      </VStack>
       <VStack gap="0">
         <Text fontSize="4xl" fontWeight={800} color="gray.0">
           {userInfo?.username || `user_${userInfo?.pk}`}
@@ -36,10 +70,7 @@ export const Info = ({ userInfo }: Props) => {
         )} */}
       </VStack>
 
-      <HStack bg="transparent">
-        <Text>{userInfo?.neuron}</Text>
-        <Text>Neurons</Text>
-      </HStack>
+      <GradeBadge neuronCount={userInfo.neuron} grade={grade} />
 
       {/* social Links */}
       {!!userInfo?.wallets[0] && (
