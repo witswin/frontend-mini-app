63ab47b8d12a28ed388804d41d4cecde17843460
Ali Maktabi <maktabi876@gmail.com>
2024-11-22 00:21:26 +0330

    removed extra script injection

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,10 +12,6 @@ export default function Document() {
       </Head>
       <body>
         <Main />
-        <Script
-          strategy="beforeInteractive"
-          src="https://telegram.org/js/telegram-web-app.js"
-        />
         <NextScript />
         <Script
           strategy="beforeInteractive"
