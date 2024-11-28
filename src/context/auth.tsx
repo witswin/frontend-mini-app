222a7968c06e6faf21862480a95a6e3f09e02699
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2024-11-28 16:39:43 +0330

    refactor: :recycle: Remove get message & sign wallet

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,5 +1,4 @@
 import { axiosClient } from '@/configs/axios';
-import { useWalletConnection } from '@/hooks/useWalletConnection';
 import {
   createContext,
   Dispatch,
@@ -8,13 +7,9 @@ import {
   useEffect,
   useState,
 } from 'react';
-import { SignableMessage } from 'viem';
-import { useAccount, useSignMessage } from 'wagmi';
-import { setCookie } from 'cookies-next';
+import { getCookie } from 'cookies-next';
 import { ACCESS_TOKEN_COOKIE_KEY } from '@/constants';
 import { UserProfile } from '@/types';
-import { useToast } from '@chakra-ui/react';
-import { handleApiError } from '@/utils';
 
 export const AuthState = createContext<UserProfile>(undefined);
 export const AuthDispatch =
@@ -24,97 +19,24 @@ interface AuthProvider extends PropsWithChildren {
   auth: UserProfile;
 }
 export const AuthProvider = ({ children, auth }: AuthProvider) => {
-  const [message, setMessage] = useState<{
-    message: SignableMessage;
-    nonce: string;
-  }>({ message: null, nonce: '' });
-
-  const { disconnect } = useWalletConnection();
-  const { signMessageAsync } = useSignMessage();
-
-  const toast = useToast();
-
-  const { isConnected, address } = useAccount();
   const [state, setState] = useState(auth);
 
-  const controller = new AbortController();
-
   useEffect(() => {
-    if (isConnected && address) {
-      axiosClient
-        .post(
-          '/auth/create-message/',
-          {
-            address,
-          },
-          { signal: controller.signal },
-        )
-        .then(({ data }) => {
-          setMessage({ message: data.message, nonce: data.nonce });
-        });
-    }
-  }, [isConnected]);
-
-  useEffect(() => {
-    // if (window.Telegram.WebApp.initData) return
-
-    if (state && state.wallets.find((item) => item.walletAddress === address))
-      return;
-
-    if (message.message) {
-      signMessageAsync({
-        message: message.message,
-        account: address,
+    if (state) return;
+    axiosClient
+      .get('/auth/info/', {
+        headers: {
+          Authorization: `TOKEN ${getCookie(ACCESS_TOKEN_COOKIE_KEY)}`,
+        },
       })
-        .then((res) => {
-          if (!window.Telegram?.WebApp?.initData)
-            return axiosClient
-              .post('/auth/verify-wallet/', {
-                address: address,
-                signature: res,
-                nonce: message.nonce,
-              })
-              .then(({ data }) => {
-                setCookie(ACCESS_TOKEN_COOKIE_KEY, data.token);
-                return data.token;
-              });
+      .then((res) => {
+        setState({ ...res.data, token: getCookie(ACCESS_TOKEN_COOKIE_KEY) });
+      })
 
-          const hasWallet = !!state.wallets.length;
-
-          return axiosClient
-            .post(hasWallet ? '/auth/change-wallets/' : '/auth/add-wallets/', {
-              address: address,
-              signature: res,
-              nonce: message.nonce,
-            })
-            .then((res) => {
-              toast({
-                description: 'Wallet added successfully',
-                status: 'success',
-              });
-              return res;
-            })
-            .catch((err) => {
-              disconnect();
-              handleApiError(err, toast);
-            });
-        })
-        .then((token) => {
-          axiosClient
-            .get('/auth/info/', {
-              headers: {
-                Authorization: `TOKEN ${token}`,
-              },
-            })
-            .then((res) => {
-              setState({ ...res.data, token });
-            });
-        })
-        .catch((err) => {
-          console.warn(err);
-        });
-    }
-  }, [message, state]);
+      .catch((err) => {
+        console.warn(err);
+      });
+  }, [state, getCookie(ACCESS_TOKEN_COOKIE_KEY)]);
 
   return (
     <AuthState.Provider value={state}>
