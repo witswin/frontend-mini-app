197ec14fbeaee7fe96e0b3f9892db7a79ffa1f1d
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2024-12-12 14:40:14 +0330

    feat: :wrench: Add toast for 500 server errors

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,5 +1,23 @@
-import axios from "axios";
+import { createStandaloneToast } from '@chakra-ui/react';
+import axios from 'axios';
 
 export const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+const { toast } = createStandaloneToast();
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 500) {
+      toast({
+        title: 'Server Error',
+        description: 'An unexpected error occurred. Please try again later.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+    return Promise.reject(error);
+  },
+);
