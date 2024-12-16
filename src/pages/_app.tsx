37c20d6ebc1ac133e3ba1ac8fa7a1b81e62269fe
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2024-12-16 20:25:20 +0330

    feat: :sparkles: Chnage route if the url has page query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,37 @@
-import { baseTheme } from "@/theme"
-import { ChakraProvider } from "@chakra-ui/react"
-import type { AppProps } from "next/app"
-import Head from "next/head"
-import "swiper/css"
-import "swiper/css/navigation"
-import "swiper/css/pagination"
-import "swiper/css/scrollbar"
-import "swiper/css/effect-coverflow"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactElement, ReactNode, useState } from "react"
-import { Layout } from "@/components/Layout"
-import { CircularPattern } from "@/components/CircularPattern"
-import { SelectedQuizProvider } from "@/modules/quiz/context"
-import { GetServerSidePropsContext, NextPage } from "next"
-import { config } from "@/configs/wagmi"
-import { WagmiProvider } from "wagmi"
-import { AuthProvider } from "@/context/auth"
-import { ACCESS_TOKEN_COOKIE_KEY } from "@/constants"
-import { axiosClient } from "@/configs/axios"
-import { TelegramAuthProvider } from "@/context/TelegramAuthProvider"
-import { AxiosAuthProvider } from "@/components/AxiosAuthProvider"
-import { UserProfile } from "@/types"
+import { baseTheme } from '@/theme';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-coverflow';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactElement, ReactNode, useState } from 'react';
+import { Layout } from '@/components/Layout';
+import { CircularPattern } from '@/components/CircularPattern';
+import { SelectedQuizProvider } from '@/modules/quiz/context';
+import { GetServerSidePropsContext, NextPage } from 'next';
+import { config } from '@/configs/wagmi';
+import { WagmiProvider } from 'wagmi';
+import { AuthProvider } from '@/context/auth';
+import { ACCESS_TOKEN_COOKIE_KEY } from '@/constants';
+import { axiosClient } from '@/configs/axios';
+import { TelegramAuthProvider } from '@/context/TelegramAuthProvider';
+import { AxiosAuthProvider } from '@/components/AxiosAuthProvider';
+import { UserProfile } from '@/types';
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-  auth: UserProfile
-}
+  Component: NextPageWithLayout;
+  auth: UserProfile;
+};
 function commonLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>
+  return <Layout>{page}</Layout>;
 }
 
 export default function App({
@@ -39,7 +39,7 @@ export default function App({
   pageProps,
   auth,
 }: AppPropsWithLayout) {
-  const getLayout = Component?.getLayout || commonLayout
+  const getLayout = Component?.getLayout || commonLayout;
 
   const [queryClient] = useState(
     () =>
@@ -47,12 +47,12 @@ export default function App({
         defaultOptions: {
           queries: {
             staleTime: 60 * 10000,
-            refetchOnMount:true,
-            retry:0
+            refetchOnMount: true,
+            retry: 0,
           },
         },
-      })
-  )
+      }),
+  );
   return (
     <>
       <Head>
@@ -78,33 +78,38 @@ export default function App({
         </WagmiProvider>
       </ChakraProvider>
     </>
-  )
+  );
 }
 
 App.getInitialProps = async ({ ctx }: { ctx: GetServerSidePropsContext }) => {
-  const cookies = ctx.req?.cookies
+  if (ctx.query?.page && ctx.res) {
+    ctx.res.writeHead(302, { Location: '/' + ctx.query.page });
+    ctx.res.end();
+    return {};
+  }
+  const cookies = ctx.req?.cookies;
   if (!!cookies) {
-    const accessToken = cookies[ACCESS_TOKEN_COOKIE_KEY]
+    const accessToken = cookies[ACCESS_TOKEN_COOKIE_KEY];
     if (!accessToken) {
       return {
         auth: null,
-      }
+      };
     }
     try {
-      const response = await axiosClient.get("/auth/info/", {
+      const response = await axiosClient.get('/auth/info/', {
         headers: {
           Authorization: `TOKEN ${accessToken}`,
         },
-      })
+      });
 
-      if (response.statusText === "OK") {
-        return { auth: { ...response.data, token: accessToken } }
+      if (response.statusText === 'OK') {
+        return { auth: { ...response.data, token: accessToken } };
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
 
-      return { auth: null }
+      return { auth: null };
     }
   }
-  return { auth: null }
-}
+  return { auth: null };
+};
