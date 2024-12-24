4dbbc352676ee5aed013b57aa970d0b318ca4ead
Ali Maktabi <maktabi876@gmail.com>
2024-12-24 19:24:16 +0330

    removed outline hovering

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,10 +1,10 @@
-import { extendTheme } from "@chakra-ui/react";
-import { colors } from "./colors";
-import { Button } from "./components/button";
-import { Tag } from "./components/tag";
-import { Badge } from "./components/badge";
-import { fontSizes } from "./fontsize";
-import { Input } from "./components/input";
+import { extendTheme } from '@chakra-ui/react';
+import { colors } from './colors';
+import { Button } from './components/button';
+import { Tag } from './components/tag';
+import { Badge } from './components/badge';
+import { fontSizes } from './fontsize';
+import { Input } from './components/input';
 
 export const baseTheme = extendTheme({
   colors,
@@ -13,37 +13,40 @@ export const baseTheme = extendTheme({
     Button,
     Tag,
     Badge,
-    Input
+    Input,
   },
   styles: {
     global: {
-      "*::-webkit-scrollbar": {
-        width: "6px",
-        height: "6px",
+      '*::-webkit-scrollbar': {
+        width: '6px',
+        height: '6px',
       },
-      "*::-webkit-scrollbar-track": {
-        background: "transparent",
+      '*::-webkit-scrollbar-track': {
+        background: 'transparent',
       },
-      "*::-webkit-scrollbar-thumb": {
-        borderRadius: "14px",
-        background: "glassBackground",
-        "&:hover": {
-          background: "rgba(217,217,217,0.5)",
+      '*::-webkit-scrollbar-thumb': {
+        borderRadius: '14px',
+        background: 'glassBackground',
+        '&:hover': {
+          background: 'rgba(217,217,217,0.5)',
+        },
+        'button, a, input, textarea': {
+          outline: 'none',
         },
       },
       body: {
-        minH: "100vh",
-        width: "100%",
-        background: "gray.800",
-        color: "gray.0",
-        fontFamily: "Montserrat",
-        position: "relative",
+        minH: '100vh',
+        width: '100%',
+        background: 'gray.800',
+        color: 'gray.0',
+        fontFamily: 'Montserrat',
+        position: 'relative',
         // pb: "106px",
-        overflowX: 'hidden'
+        overflowX: 'hidden',
       },
       html: {
-        height: "100%",
-        overflowX: 'hidden'
+        height: '100%',
+        overflowX: 'hidden',
       },
     },
   },
